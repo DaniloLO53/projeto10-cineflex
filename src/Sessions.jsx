e9b0c03a17a997b329270f1b9970b81976bcb03c
Danilo Leão <olin_ad@hotmail.com>
2022-12-06 18:58:10 -0300

    session infos layout

--- src/Sessions.jsx
+++ src/Sessions.jsx
@@ -2,20 +2,22 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import styled from 'styled-components';
 
 function Sessions() {
-  const { idFilme } = useParams();
+  const idInfo = useParams();
   const [sessions, setSessions] = useState(null);
 
   useEffect(() => {
-    const URL = `https://mock-api.driven.com.br/api/v8/cineflex/movies/${idFilme}/showtimes`;
+    const URL = `https://mock-api.driven.com.br/api/v8/cineflex/movies/${idInfo.idFilme}/showtimes`;
     const controller = new AbortController();
     const { signal } = controller;
 
     const fetchSessions = async () => {
       try {
         const sessionsInfo = await axios.get(URL, signal);
-        setSessions(sessionsInfo);
+        setSessions(sessionsInfo.data);
+        console.log(sessions);
       } catch (error) {
         alert(error.message);
         throw new Error(error);
@@ -30,10 +32,50 @@ function Sessions() {
   }, []);
 
   return (
-    <div>
-      oi
-    </div>
+    <StyledSessions>
+      {(sessions?.days || []).map(({
+        id,
+        weekday,
+        date,
+        showtimes,
+      }) => (
+        <StyledSession key={id}>
+          <StyledDate>
+            {weekday}
+            {' '}
+            -
+            {' '}
+            {date}
+          </StyledDate>
+          <StyledTime>
+            {showtimes.map(({ name, id: idTime }) => (
+              <button type="button" key={idTime}>{name}</button>
+            ))}
+          </StyledTime>
+        </StyledSession>
+      ))}
+    </StyledSessions>
   );
 }
+
+const StyledSessions = styled.div`
+  background-color: red;
+  width: 100%;
+`;
+
+const StyledSession = styled.div`
+  background-color: green;
+  width: 100%;
+`;
+
+const StyledDate = styled.div`
+  background-color: purple;
+  width: 100%;
+`;
+
+const StyledTime = styled.div`
+  background-color: yellow;
+  width: 100%;
+`;
 
 export default Sessions;
