1414370a24ff774989506633830df4ab5cb40bfa
Danilo Leão <olin_ad@hotmail.com>
2022-12-06 16:38:16 -0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div>
-      re
-    </div>
+    <StyledAppContainer>
+      <StyledHeader>
+        CINEFLEX
+      </StyledHeader>
+    </StyledAppContainer>
   );
 }
+
+const StyledAppContainer = styled.div`
+  font-family: 'Roboto', sans-serif;
+`;
+
+const StyledHeader = styled.div`
+  width: 100%;
+  background-color: #c3cfd9;
+  color: #e8833a;
+  font-size: 37px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+`;
 
 export default App;
