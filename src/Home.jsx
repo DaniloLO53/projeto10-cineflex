2f1407c369bce4ed42b92a13c648bf7a56371642
Danilo Leão <olin_ad@hotmail.com>
2022-12-06 18:05:07 -0300

    movies images layout

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
 function Home() {
+  const [movies, setMovies] = useState(null);
+
   useEffect(() => {
     const URL = 'https://mock-api.driven.com.br/api/v8/cineflex/movies';
     const controller = new AbortController();
@@ -9,8 +12,9 @@ function Home() {
 
     const fetchMovies = async () => {
       try {
-        const movies = await axios.get(URL, { signal });
-        console.log(movies);
+        const { data } = await axios.get(URL, { signal });
+        console.log(data);
+        setMovies(data);
       } catch (error) {
         alert(error.message);
         throw new Error(error);
@@ -25,10 +29,46 @@ function Home() {
   }, []);
 
   return (
-    <div>
-      oi
-    </div>
+    <StyledMovies>
+      {(movies || []).map(({ posterURL, title, id }) => (
+        <StyledButton
+          key={id}
+          type="button"
+        >
+          <figure>
+            <img alt={title} src={posterURL} />
+          </figure>
+        </StyledButton>
+      ))}
+    </StyledMovies>
   );
 }
+
+const StyledMovies = styled.div`
+  /* background-color: red; */
+  max-width: 100vw;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const StyledButton = styled.div`
+  width: 159px;
+  height: 200px;
+  margin: 20px;
+  padding: 8px;
+  border-radius: 3px;
+  box-shadow: 0 2px 4px 2px rgba(0, 0, 0, 0.1);
+
+  figure {
+    width: 100%;
+    height: 100%;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+`;
 
 export default Home;
